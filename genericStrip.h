db701af7fa23af8439d9fc854c994fc557cccfec
Thomas SOETE <github@alkorin.fr>
2020-11-29 12:30:50 +0100

    Implement other NeoPixelBus methods

--- genericStrip.h
+++ genericStrip.h
@@ -24,16 +24,42 @@ class GenericStrip
 {
 public:
     virtual void Begin() = 0;
+
     virtual void Show() = 0;
+    virtual bool CanShow() const = 0;
+
+    virtual bool IsDirty() const = 0;
+    virtual void Dirty() = 0;
+    virtual void ResetDirty() = 0;
+
+    virtual uint8_t *Pixels() = 0;
+    virtual size_t PixelsSize() const = 0;
+    virtual size_t PixelSize() const = 0;
+    virtual uint16_t PixelCount() const = 0;
+
     virtual void SetPixelColor(uint16_t indexPixel, RgbColor const &color) = 0;
     virtual void SetPixelColor(uint16_t indexPixel, RgbwColor const &color) = 0;
+    virtual RgbColor GetPixelRgbColor(uint16_t indexPixel) const = 0;
+    virtual RgbwColor GetPixelRgbwColor(uint16_t indexPixel) const = 0;
 
     virtual void ClearTo(RgbColor const &color) = 0;
     virtual void ClearTo(RgbwColor const &color) = 0;
     virtual void ClearTo(RgbColor const &color, uint16_t first, uint16_t last) = 0;
     virtual void ClearTo(RgbwColor const &color, uint16_t first, uint16_t last) = 0;
 
-    virtual uint16_t PixelCount() const = 0;
+    virtual void RotateLeft(uint16_t rotationCount) = 0;
+    virtual void RotateLeft(uint16_t rotationCount, uint16_t first, uint16_t last) = 0;
+
+    virtual void ShiftLeft(uint16_t shiftCount) = 0;
+    virtual void ShiftLeft(uint16_t shiftCount, uint16_t first, uint16_t last) = 0;
+
+    virtual void RotateRight(uint16_t rotationCount) = 0;
+    virtual void RotateRight(uint16_t rotationCount, uint16_t first, uint16_t last) = 0;
+
+    virtual void ShiftRight(uint16_t shiftCount) = 0;
+    virtual void ShiftRight(uint16_t shiftCount, uint16_t first, uint16_t last) = 0;
+
+    virtual void SwapPixelColor(uint16_t indexPixelOne, uint16_t indexPixelTwo) = 0;
 };
 
 template <typename T_COLOR_FEATURE, typename T_METHOD>
@@ -42,22 +68,81 @@ class Strip : public GenericStrip
 public:
     Strip(int count) : _strip(count){};
 
-    void Begin() { _strip.Begin(); };
-    void Show() { _strip.Show(); };
+    void Begin()
+    {
+        _strip.Begin();
+    };
+
+    void Show()
+    {
+        _strip.Show();
+    };
+
+    bool CanShow() const
+    {
+        return _strip.CanShow();
+    };
+
+    bool IsDirty() const
+    {
+        return _strip.IsDirty();
+    };
+
+    void Dirty()
+    {
+        _strip.Dirty();
+    };
+
+    void ResetDirty()
+    {
+        _strip.ResetDirty();
+    };
+
+    uint8_t *Pixels()
+    {
+        return _strip.Pixels();
+    }
+
+    size_t PixelsSize() const
+    {
+        return _strip.PixelsSize();
+    };
+
+    size_t PixelSize() const
+    {
+        return _strip.PixelSize();
+    };
+
+    uint16_t PixelCount() const
+    {
+        return _strip.PixelCount();
+    };
 
     void SetPixelColor(uint16_t indexPixel, RgbColor const &color)
     {
         _strip.SetPixelColor(indexPixel, convertColor<typename T_COLOR_FEATURE::ColorObject>(color));
     }
+
     void SetPixelColor(uint16_t indexPixel, RgbwColor const &color)
     {
         _strip.SetPixelColor(indexPixel, convertColor<typename T_COLOR_FEATURE::ColorObject>(color));
+    };
+
+    RgbColor GetPixelRgbColor(uint16_t indexPixel) const
+    {
+        return convertColor<RgbColor>(_strip.GetPixelColor(indexPixel));
+    };
+
+    RgbwColor GetPixelRgbwColor(uint16_t indexPixel) const
+    {
+        return convertColor<RgbwColor>(_strip.GetPixelColor(indexPixel));
     };
 
     void ClearTo(RgbColor const &color)
     {
         _strip.ClearTo(convertColor<typename T_COLOR_FEATURE::ColorObject>(color));
     }
+
     void ClearTo(RgbwColor const &color)
     {
         _strip.ClearTo(convertColor<typename T_COLOR_FEATURE::ColorObject>(color));
@@ -72,10 +157,50 @@ public:
         _strip.ClearTo(convertColor<typename T_COLOR_FEATURE::ColorObject>(color), first, last);
     };
 
-    uint16_t PixelCount() const
+    void RotateLeft(uint16_t rotationCount)
     {
-        return _strip.PixelCount();
-    }
+        _strip.RotateLeft(rotationCount);
+    };
+
+    void RotateLeft(uint16_t rotationCount, uint16_t first, uint16_t last)
+    {
+        _strip.RotateLeft(rotationCount, first, last);
+    };
+
+    void ShiftLeft(uint16_t shiftCount)
+    {
+        _strip.ShiftLeft(shiftCount);
+    };
+
+    void ShiftLeft(uint16_t shiftCount, uint16_t first, uint16_t last)
+    {
+        _strip.ShiftLeft(shiftCount, first, last);
+    };
+
+    void RotateRight(uint16_t rotationCount)
+    {
+        _strip.RotateRight(rotationCount);
+    };
+
+    void RotateRight(uint16_t rotationCount, uint16_t first, uint16_t last)
+    {
+        _strip.RotateRight(rotationCount, first, last);
+    };
+
+    void ShiftRight(uint16_t shiftCount)
+    {
+        _strip.ShiftRight(shiftCount);
+    };
+
+    void ShiftRight(uint16_t shiftCount, uint16_t first, uint16_t last)
+    {
+        _strip.ShiftRight(shiftCount, first, last);
+    };
+
+    void SwapPixelColor(uint16_t indexPixelOne, uint16_t indexPixelTwo)
+    {
+        _strip.SwapPixelColor(indexPixelOne, indexPixelTwo);
+    };
 
 private:
     NeoPixelBus<T_COLOR_FEATURE, T_METHOD> _strip;
